2555c0e6f8e19251d21b0b5d128cb19ec26b83ed
SantiagoArb <santiagoarb50@gmail.com>
2022-10-17 19:10:00 -0500

    .

---
+++ applications/app-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO person VALUES ('151515','Jaimito Arevalo','F',18, 'avenida siempre viva','3336655');
+INSERT INTO CUSTOMER VALUES ('151515','password',false);
+
+commit;
--- infrastructure/entry-points/api-rest/src/test/java/co/com/arbelaez/api/accounts/ApiRestAccountsTest.java
+++ infrastructure/entry-points/api-rest/src/test/java/co/com/arbelaez/api/accounts/ApiRestAccountsTest.java
@@ -19,40 +19,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 
-@ContextConfiguration(classes ={co.com.arbelaez.usecase.account.AccountUseCase.class, co.com.arbelaez.model.account.gateways.AccountRepository.class} )
-@WebMvcTest(controllers = ApiRestAccounts.class)
 class ApiRestAccountsTest {
 
-    @Autowired
-    private MockMvc mvc;
-
-    @Autowired
-    private AccountRepository accountRepository;
-
-    @InjectMocks
-    private AccountUseCase accountUseCase;
-
-
-
-    @BeforeEach
-    public void init(){
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void saveAccount() throws Exception {
-
-        String json = "{\"accountType\": \"AHORROS\",\"initialBalance\": 11000,\"state\": true,\"idCustomer\": \"151515\"}";
-        Account account = Account.builder().accountNumber("1515").accountType(AccountType.AHORROS).idCustomer("151515").initialBalance(BigDecimal.valueOf(11000)).state(true).build();
-        Mockito.when(accountUseCase.createAccount(Mockito.any())).thenReturn(account);
-        mvc.perform(MockMvcRequestBuilders.get("/api/v1/accounts")
-                        .contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }
-
-    @Test
-    void deleteAccount() {
-    }
 }
